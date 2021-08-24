57e222c86d0367b12e0cf3103d35d3ce28afc3da
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2021-08-24 17:02:42 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,46 +5,82 @@ import Map from './components/Map/Map';
 import {CssBaseline, Grid} from '@material-ui/core';
 import {getPlacesData} from './api'
 
-
 const App = () => {
     const [coordinates, setCoordinates] = useState({});
     const [bounds, setBounds] = useState({});
     //to set and get the place where you are 
     const [places, setPlaces] = useState([]);
-
+    const [type, setType] = useState('restaurants')
+    const [rating,setRating] = useState('');
+    const [filterplaces, setFilterplacer]= useState([]);
+    const [weatherData, setWeatherData] = useState([]);
     const [childClicked, setChildClicked] = useState(null);
-    const [isLoading,setisLoading] = useState(false);
+
+    const [autocomplete, setAutocomplete] = useState(null);
+    const [isLoading,setIsLoading] = useState(false);
   
-    useEffect (()=> {
-        navigator.geolocation.getCurrentPosition(({coords:{latitude,longitude} } ) => {
-            setCoordinates({lat: latitude, lng: longitude});
-        })
-    },[])
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
+          setCoordinates({ lat: latitude, lng: longitude });
+        });
+      }, []);
+
+    useEffect( () =>{
+        const  filterplaces = places.filter((places)=> Number(places.rating) > rating);
+        setFilterplacer(filterplaces);
+
+    },[rating]);
 
     //using the effect to unwrapp the data of the restaurants
     useEffect ( () => {
-        setisLoading(true);
-        //console.log(coordinates, bounds)
-        getPlacesData(bounds.sw,bounds.ne)
-        .then((data) => {
-            //consolelog the data 
-            //console.log(data);
-            //the set data to the places
-            setPlaces(data)
-            setisLoading(false);
+        if(bounds.sw && bounds.ne){
+            setIsLoading(true);
+            //to get the weather data
+            getWeatherData(coordinates.lat, coordinates.lng)
+            .then((data) => setWeatherData(data));
+            //console.log(coordinates, bounds)
+            getPlacesData(type,bounds.sw,bounds.ne)
+            .then((data) => {
+                //consolelog the data 
+                //console.log(data);
+                //the set data to the places
+                setPlaces(data?.filter((place)=>place.name && place.num_review > 0))
+                setFilterplacer([]);
+                setIsLoading(false);
+                setRating('');
+    
+            })
 
-        })
+        }
+    },[type, bounds])
 
-    },[coordinates, bounds])
+    const onLoad = (autoC) => setAutocomplete(autoC);
+
+  const onPlaceChanged = () => {
+    const lat = autocomplete.getPlace().geometry.location.lat();
+    const lng = autocomplete.getPlace().geometry.location.lng();
+
+    setCoordinates({ lat, lng });
+  };
+
+   // console.log(places);
+    //console.log(PlaceDetails);
+   
 
     return (
         <>
         <CssBaseline/>
-        <Header/>
+        <Header onPlaceChanged={onPlaceChanged} onLoad={onLoad}/>
         <Grid container spacing={3} style={{width:'100%'}}>
             <Grid item xs={12} md={4}>
-                <List places = {places}
+                <List 
+                places = { filterplaces.length ? filterplaces : places}
                 childClicked={childClicked}
+                isLoading={isLoading}
+                type={type}
+                setType={setType}
+                rating={rating}
+                setRating={setRating}
                 />
             </Grid>
 
@@ -54,10 +90,11 @@ const App = () => {
                 setBounds = {setBounds}
                 coordinates = {coordinates}
                 isLoading={isLoading}
-                places={places}
+                setChildClicked={setChildClicked}
+                places={filterplaces.length ? filterplaces : places}
+                weatherData={weatherData}
                 />
             </Grid>
-            
         </Grid>
         </>
     )
