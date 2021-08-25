ed8c9c9e31ce2a82457957d4f9a8f68a0a7f87d8
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2021-08-25 14:09:19 -0500

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
-
-export const getPlacesData = async ( sw, ne) => {
+//const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
+//const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
+export const getPlacesData = async (type, sw, ne) => {
     try {
-        const {data: { data } } = await axios.get (URL,{
+        const {data: { data } } = await axios.get (`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,{
             params: {
               bl_latitude: sw.lat,
               bl_longitude: sw.lng,
@@ -12,16 +12,34 @@ export const getPlacesData = async ( sw, ne) => {
               tr_latitude: ne.lat,
             },
             headers: {
-              'x-rapidapi-key': 'c99cc830e0msh76f422f629600a2p15a917jsn2d05516f88d4',
+              'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_API_KEY,
+              //process.env.REACT_APP_RAPIDAPI_TRAVEL_API_KEY,
               'x-rapidapi-host': 'travel-advisor.p.rapidapi.com'
             }
+           
           
         });
 
         return data;
+       // console.log(data)
 
     } catch (error) {
         console.log(error)
 
     }
 }
+
+export const getWeatherData = async (lat,lng) => {
+  try {
+    const {data: { data } } = await axios.get ('https://community-open-weather-map.p.rapidapi.com/find',{
+      params: { lon: lng ,lat:lat },
+      headers: {
+        'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_API_KEY,
+        'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+      },
+    });
+
+    return data;
+
+} catch (error) {
+    console.log(error)
